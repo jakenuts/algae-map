d5834524f3e024add9e0dd9c1bf8990422c3181f
James White <james@gbase.com>
2024-10-04 12:41:11 -0700

    Removed unused import

--- src/features/algaeBloom/MapLayers.tsx
+++ src/features/algaeBloom/MapLayers.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useMap, LayersControl, TileLayer } from 'react-leaflet';
 
 const MapLayers = () => {
